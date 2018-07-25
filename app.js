b4e03bca55d4d0792384b1e81d02d6fc43ed3b99
MIClavelTe <miclavelte@gmail.com>
2018-07-24 18:26:03 -0700

    Add route functions

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
+  router.home(req, res)
+  router.user(req, res);
 });
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
